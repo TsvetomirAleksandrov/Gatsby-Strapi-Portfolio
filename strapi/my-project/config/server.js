61499d4df60a67d189e9d779b7a5972da85027c2
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-07-23 12:44:20 +0300

    Initial commit

---
+++ strapi/my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e23cc471e23ce5606ae99988bc7102f'),
+    },
+  },
+});
---
+++ strapi/my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '174523f4-eb62-4ccc-9fc7-e1a5da850360'
+};
